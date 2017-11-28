d52107ff4b230ede8db5c4a31d74930542e5369c
Umut <umut.tontus@sahibinden.com>
2017-11-28 10:02:18 +0300

    Filter feature added as plugin impl

--- app/src/main/java/iammert/com/expandablelayout/FruitCategory.java
+++ app/src/main/java/iammert/com/expandablelayout/FruitCategory.java
@@ -1,23 +1,14 @@
 package iammert.com.expandablelayout;
 
-import android.support.annotation.NonNull;
-
-import iammert.com.expandablelib.Filterable;
-
 /**
  * Created by mertsimsek on 28/07/2017.
  */
 
-public class FruitCategory implements Filterable{
+public class FruitCategory {
     public String name;
 
     public FruitCategory(String name) {
         this.name = name;
     }
 
-    @NonNull
-    @Override
-    public String getKey() {
-        return name;
-    }
 }
