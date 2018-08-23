b592665b447968cb590dbcaf5aa9243eccf7e501
Ray Li <ray.li.verified@gmail.com>
2018-08-23 14:39:42 -0500

    Update Android X
    *Update dependencies to Android X.
    *Update SDK v28.

--- app/src/main/java/iammert/com/expandablelayout/MainActivity.java
+++ app/src/main/java/iammert/com/expandablelayout/MainActivity.java
@@ -1,7 +1,7 @@
 package iammert.com.expandablelayout;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
